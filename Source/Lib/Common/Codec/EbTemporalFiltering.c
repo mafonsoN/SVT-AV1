72c5e3e49a5332837c7d4c923426541ae356784e
mafonsoN <mafonso@netflix.com>
2019-03-29 14:50:22 -0700

    implementation of some of the filtering functions

--- Source/Lib/Common/Codec/EbTemporalFiltering.c
+++ Source/Lib/Common/Codec/EbTemporalFiltering.c
@@ -20,9 +20,206 @@
 #include "EbTemporalFiltering.h"
 
 #define MAX_FRAMES_TO_FILTER 10
-#define EDGE_THRESHOLD 50 // from libaom
-#define SQRT_PI_BY_2 1.25331413732 // from libaom
+#define EDGE_THRESHOLD 50
+#define SQRT_PI_BY_2 1.25331413732
 #define SMOOTH_THRESHOLD 16
+// Block size used in temporal filtering
+#define TF_BLOCK BLOCK_32X32
+#define BH 32
+#define BW 32
+#define BLK_PELS 1024  // Pixels in the block
+#define THR_SHIFT 2
+#define TF_SUB_BLOCK BLOCK_16X16
+#define SUB_BH 16
+#define SUB_BW 16
+
+static INLINE int get_filter_weight(unsigned int i,
+                                    unsigned int j,
+                                    unsigned int block_height,
+                                    unsigned int block_width,
+                                    const int *blk_fw,
+                                    int use_32x32) {
+
+    if (use_32x32)
+        // blk_fw[0] ~ blk_fw[3] are the same.
+        return blk_fw[0];
+
+    int filter_weight = 0;
+    if (i < block_height / 2) {
+        if (j < block_width / 2)
+            filter_weight = blk_fw[0];
+        else
+            filter_weight = blk_fw[1];
+    } else {
+        if (j < block_width / 2)
+            filter_weight = blk_fw[2];
+        else
+            filter_weight = blk_fw[3];
+    }
+    return filter_weight;
+
+}
+
+// Only used in single plane case
+void apply_filtering(uint8_t *frame1,
+                    unsigned int stride,
+                    uint8_t *frame2,
+                    unsigned int block_width,
+                    unsigned int block_height,
+                    int strength,
+                    const int *blk_fw,
+                    int use_32x32,
+                    unsigned int *accumulator,
+                    uint16_t *count) {
+
+    unsigned int i, j, k;
+    int modifier;
+    int byte = 0;
+    const int rounding = strength > 0 ? 1 << (strength - 1) : 0;
+
+    for (i = 0, k = 0; i < block_height; i++) {
+        for (j = 0; j < block_width; j++, k++) {
+            int pixel_value = *frame2;
+            int filter_weight =
+                    get_filter_weight(i, j, block_height, block_width, blk_fw, use_32x32);
+
+            // non-local mean approach
+            int diff_sse[9] = { 0 };
+            int idx, idy, index = 0;
+
+            for (idy = -1; idy <= 1; ++idy) {
+                for (idx = -1; idx <= 1; ++idx) {
+                    int row = (int)i + idy;
+                    int col = (int)j + idx;
+
+                    if (row >= 0 && row < (int)block_height && col >= 0 &&
+                        col < (int)block_width) {
+                        int diff = frame1[byte + idy * (int)stride + idx] -
+                                   frame2[idy * (int)block_width + idx];
+                        diff_sse[index] = diff * diff;
+                        ++index;
+                    }
+                }
+            }
+
+            assert(index > 0);
+
+            modifier = 0;
+            for (idx = 0; idx < 9; ++idx) modifier += diff_sse[idx];
+
+            modifier *= 3;
+            modifier /= index;
+
+            ++frame2;
+
+            modifier += rounding;
+            modifier >>= strength;
+
+            if (modifier > 16) modifier = 16;
+
+            modifier = 16 - modifier;
+            modifier *= filter_weight;
+
+            count[k] += modifier;
+            accumulator[k] += modifier * pixel_value;
+
+            byte++;
+        }
+
+        byte += stride - block_width;
+    }
+}
+
+// buf_stride is the block size: 32 for Y and 16 for U and V
+static void apply_filtering_self(const uint8_t *pred,
+                                 int blk_luma_height,
+                                 int blk_luma_width,
+                                 int blk_chroma_height,
+                                 int blk_chroma_width,
+                                 uint32_t *accumulator,
+                                 uint16_t *count) {
+
+    int filter_weight = 2;
+    const int modifier = filter_weight * 16; // TODO: defines with these constants
+    unsigned int i, j, k = 0;
+    int block_height = blk_luma_height;
+    int block_width = blk_luma_width;
+    int buf_stride = blk_luma_height;
+
+    for (i = 0; i < block_height; i++) {
+        for (j = 0; j < block_width; j++) {
+            const int pixel_value = pred[i * buf_stride + j];
+            count[k] += modifier;
+            accumulator[k] += modifier * pixel_value;
+            ++k;
+        }
+    }
+
+}
+
+// Produce the filtered alt-ref picture
+static void produce_temporally_filtered_pic(EbPictureBufferDesc_t *input_picture_ptr,
+                                            uint8_t altref_strength,
+                                            uint8_t altref_nframes) {
+
+    int frame_index;
+    DECLARE_ALIGNED(16, uint32_t, accumulator[BLK_PELS * 3]);
+    DECLARE_ALIGNED(16, uint16_t, count[BLK_PELS * 3]);
+    DECLARE_ALIGNED(32, uint8_t, predictor[BLK_PELS * 3]);
+    const int blk_chroma_height = BH >> 1; // TODO: implement for 420 now and extend it later
+    const int blk_chroma_width = BW >> 1;
+    int index_center = (altref_nframes - 1) >> 1;
+    int blk_row, blk_col;
+    int blk_cols = (input_picture_ptr->width + BW - 1) / BW; // I think only the part of the picture
+    int blk_rows = (input_picture_ptr->height + BH - 1) / BH; // that fits to the 32x32 blocks are actually filtered
+    EbByte src;
+    int stride_y = input_picture_ptr->stride_y;
+
+    // index of the center frame
+    index_center += (altref_nframes + 1) & 0x1;
+
+    // first position of the buffer
+    src = input_picture_ptr->buffer_y +
+          input_picture_ptr->origin_y*input_picture_ptr->stride_y +
+          input_picture_ptr->origin_x;
+    
+    // for each block
+    for (blk_row = 0; blk_row < blk_rows; blk_row++) {
+
+        for (blk_col = 0; blk_col < blk_cols; blk_col++) {
+
+            // reset accumulator and count
+            memset(accumulator, 0, BLK_PELS * 3 * sizeof(accumulator[0]));
+            memset(count, 0, BLK_PELS * 3 * sizeof(count[0]));
+
+            // for every frame to filter
+            for(frame_index=0; frame_index < (int)altref_nframes; frame_index++){
+
+                // Step 1: motion compensation TODO: motion compensation
+                // Just for testing purposes - copy the original block (Y only now)
+                memcpy(predictor, src + blk_col * BH * stride_y + blk_row, BLK_PELS * sizeof(uint8_t));
+
+                // Step 2: temporal filtering using the motion compensated blocks
+
+                // if frame to process is the center frame
+                if (frame_index == 0) { // delete
+                //if (frame_index == index_center) {
+
+                    apply_filtering_self(predictor,
+                                         BH,
+                                         BW,
+                                         blk_chroma_height,
+                                         blk_chroma_width,
+                                         accumulator,
+                                         count);
+
+                }
+
+            }
+        }
+    }
+
+}
 
 // This is an adaptation of the mehtod in the following paper:
 // Shen-Chuan Tai, Shih-Ming Yang, "A fast method for image noise
@@ -240,6 +437,8 @@ EbErrorType init_temporal_filtering(PictureParentControlSet_t *picture_control_s
         //frames[frames_to_blur - 1 - frame] = &buf->img;
     }
 
+    produce_temporally_filtered_pic(input_picture_ptr, altref_strength, altref_nframes);
+
     // Initialize errorperbit, sadperbit16 and sadperbit4.
     // rdmult = av1_compute_rd_mult_based_on_qindex(cpi, ARNR_FILT_QINDEX);
     // set_error_per_bit(&cpi->td.mb, rdmult);
@@ -248,6 +447,8 @@ EbErrorType init_temporal_filtering(PictureParentControlSet_t *picture_control_s
 
     // function that implements the temporal filtering in libaom
     //temporal_filter_iterate_c(cpi, frames, frames_to_blur, frames_to_blur_backward, strength, &sf);
+
+    return EB_ErrorNone;
 }
 
 void init_altRefs(){
