f9cf7c7a999e9041471d5ccfc1facccf3c81d164
Mariana Fernandez Afonso <mafonso@netflix.com>
2019-05-20 15:50:46 -0700

    adding sse SIMD for the temporal filtering (not bulding yet)

--- Source/Lib/Common/Codec/EbTemporalFiltering.h
+++ Source/Lib/Common/Codec/EbTemporalFiltering.h
@@ -22,8 +22,59 @@
 #include "EbSequenceControlSet.h"
 #include "EbDefinitions.h"
 #include "EbBitstreamUnit.h"
+#include "EbTemporalFiltering_sse4.c"
 
 #define ALTREF_MAX_NFRAMES 10
+
+#define COLOR_CHANNELS 3
+#define C_Y 0
+#define C_U 1
+#define C_V 2
+
+#define EDGE_THRESHOLD 50
+#define SQRT_PI_BY_2 1.25331413732
+#define SMOOTH_THRESHOLD 16
+// Block size used in temporal filtering
+#define BW 64
+#define BH 64
+#define BW_CH BW>>1
+#define BH_CH BH>>1
+#define BLK_PELS 4096  // Pixels in the block
+#define N_16X16_BLOCKS 16
+#define N_32X32_BLOCKS 4
+
+#define INT_MAX 2147483647 //max value for an int
+#define INT_MIN (-2147483647-1) //min value for an int
+#define THR_SHIFT 2 // should be 2
+
+#define INIT_WEIGHT 2
+#define WEIGHT_MULTIPLIER 16
+
+// ALT-REF debug-specific defines
+#define DEBUG_TEMPORAL_FILTER 0
+#define DEBUG_MC 0
+#define VANILA_ME 0
+#define LIBAOM_FILTERING 0
+#define MC_CHROMA 1
+#define USE_ONLY_16X16 1
+#define USE_ONLY_32X32 0
+#define LIBAOM_VF 1
+
+#if LIBAOM_VF
+#define THRES_LOW 10000
+#define THRES_HIGH 20000
+#define THRES_DIFF_LOW 6000
+#define THRES_DIFF_HIGH 12000
+#else
+#define THRES_LOW 768 // mean SAD of 3
+#define THRES_HIGH 1280 // mean SAD of 5
+#define THRES_DIFF_LOW 300
+#define THRES_DIFF_HIGH 450
+#endif
+
+#define _MM_HINT_T2  1
+#define OD_DIVU_DMAX (1024)
+
 #if MOVE_TF
 
 
